484704a9dd169b525a9b37457a216ab9b3bec2ad
Jeanpierre Aguilar <76506507+JeanpierreAguilar15@users.noreply.github.com>
2025-11-16 17:00:36 -0500

    Delete SistemaWebLaboratorio/Software/database directory

--- SistemaWebLaboratorio/Software/database/migrations/002_seed_roles.sql
+++
@@ -1,5 +0,0 @@
--- inserta roles mínimos, idempotente
-insert into usuario.roles (nombre_rol) values ('PACIENTE') on conflict do nothing;
-insert into usuario.roles (nombre_rol) values ('PERSONAL_LAB') on conflict do nothing;
-insert into usuario.roles (nombre_rol) values ('ADMIN') on conflict do nothing;
-
