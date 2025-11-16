8e8370e72da5f267863d5f5866db3d7ac5263629
Jeanpierre Aguilar <76506507+JeanpierreAguilar15@users.noreply.github.com>
2025-11-16 17:00:19 -0500

    Delete SistemaWebLaboratorio/Software/backend directory

--- SistemaWebLaboratorio/Software/backend/src/modules/catalog/catalog.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CatalogController } from './catalog.controller';
-import { CatalogService } from './catalog.service';
-import { CatalogPublicController } from './catalog.public.controller';
-
-@Module({ controllers:[CatalogController, CatalogPublicController], providers:[CatalogService] })
-export class CatalogModule {}
